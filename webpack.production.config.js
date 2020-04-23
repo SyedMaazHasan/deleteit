de09ad1728ea119691e77b84839c1ba909b1937b
Syed Maaz Hasan <49359456+SyedMaazHasan@users.noreply.github.com>
2020-04-23 12:58:24 +0530

    Create webpack.production.config.js

---
+++ webpack.production.config.js
@@ -0,0 +1,119 @@
+"use strict";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
+//const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+var CompressionPlugin = require("compression-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+var Visualizer = require("webpack-visualizer-plugin");
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  context: path.resolve("src"),
+  mode: "production",
+  entry: {
+    main: "./index",
+  },
+  output: {
+    path: path.resolve("prodbuild"),
+    publicPath: "/",
+    filename: "public/[name].[chunkhash].js", //this will provide hash value automatically
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "node_modules")],
+    extensions: [".ts", ".js", ".tsx", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)?$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            babelrc: false,
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    chrome: "54",
+                    ie: "11",
+                  },
+                },
+              ],
+              "@babel/preset-react",
+            ],
+            plugins: [
+              ["@babel/plugin-proposal-class-properties", { loose: true }],
+              "react-hot-loader/babel",
+            ],
+            env: {
+              production: {
+                presets: ["minify"],
+              },
+            },
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [{ loader: "raw-loader" }],
+      },
+      {
+        test: /\.(gif|jpe?g|png|ico)$/,
+        use: [{ loader: "url-loader" }],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [{ loader: "file-loader" }],
+      },
+      {
+        test: /\.svg$/,
+        use: [{ loader: "url-loader" }],
+      },
+      {
+        test: /\.(p)?css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+    ],
+  },
+  optimization: {
+    //generates a seperate bundle for dependencies
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: "all",
+        },
+      },
+    },
+  },
+  plugins: [
+    new Visualizer(),
+    new webpack.DefinePlugin({
+      //declares a global env variable when bundle loads
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+    new CompressionPlugin(), //zip the whole bundle
+    // new ExtractTextPlugin("public/css/[name].[hash].css"), //generates new bundle for css
+    new OptimizeCssAssetsPlugin(),
+    // new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+    // new BundleAnalyzerPlugin({
+    //   analyzerMode: 'static',
+    // }),
+    new HtmlWebpackPlugin({
+      //generates index.html with proper reference to <script> tag for js and css
+      template: path.resolve("public/index.html"),
+      //favicon: path.resolve('src/public/images/favicon-4.ico'),
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+      },
+    }),
+  ],
+};
