99a3674020ed7bd7ee6a625ceea7187f95e3c111
Syed Maaz Hasan <49359456+SyedMaazHasan@users.noreply.github.com>
2020-12-05 20:29:32 +0530

    Update lambda.js

--- lambda.js
+++ lambda.js
@@ -1,3 +1,11 @@
+//Lambda func structure
+exports.handler = async function(event, context, callback) {
+	
+}
+
+
+
+//ASYNC FUNCTION
 exports.handler = async function(event, context, callback) {
 	
   console.log('source which triggered this func passed query string param: ', event.key1);
@@ -5,5 +13,16 @@ exports.handler = async function(event, context, callback) {
   console.log('Remaining time: ', context.getRemainingTimeInMillis())
   console.log('Function name: ', context.functionName)
   
-  return context.logStreamName
+  return callback(null, "maaz")
+}
+
+//SYNCHRONOUS FUNCTION
+exports.handler = function(event, context) {
+	
+  console.log('source which triggered this func passed query string param: ', event.key1);
+  
+  console.log('Remaining time: ', context.getRemainingTimeInMillis())
+  console.log('Function name: ', context.functionName)
+  
+  return "maaz"
 }
