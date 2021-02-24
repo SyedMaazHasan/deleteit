92188e3fed470b8606a45a4b2a1e95fd3f85efd6
Syed Maaz Hasan <49359456+SyedMaazHasan@users.noreply.github.com>
2021-02-24 12:07:30 +0530

    Create delete it.js

---
+++ delete it.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+class EventElement extends Component {
+  state = {};
+
+  handleparentButtonClicked = (e) => {
+    console.log("parent clicked");
+  };
+  handleChildButtonClicked = (e) => {
+    console.log(e);
+    e.stopPropagation();
+    console.log("child clicked");
+  };
+  render() {
+    return (
+      <button onClick={(e)=>this.handleparentButtonClicked(e)}>
+        parent button
+        <button onClick={(e) => this.handleChildButtonClicked(e)}>
+          child button
+        </button>
+      </button>
+    );
+  }
+}
+
+export default EventElement;
